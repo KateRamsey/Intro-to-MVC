f52e1892abf13a4b6d0e0d7a267368d624d48fed
Kate Ramsey <kateramsey@live.com>
2016-04-04 14:16:24 -0500

    Add classmates model, controller and view

---
+++ Intro to MVC/Controllers/ClassmatesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Intro_to_MVC.Models;
+
+namespace Intro_to_MVC.Controllers
+{
+    public class ClassmatesController : Controller
+    {
+        // GET: Classmates
+        public ActionResult Index()
+        {
+            var classmates = new List<Classmate>
+            {
+                new Classmate {Name = "Kate", HairColor = "Dirty Blonde", Height = 66, Id = 1},
+                new Classmate {Name = "Brian", HairColor = "Black", Height = 66, Id = 2},
+                new Classmate {Name = "Kevin", HairColor = "Brown", Height = 66, Id = 3},
+                new Classmate {Name = "Trey", HairColor = "Brown", Height = 66, Id = 4},
+                new Classmate {Name = "Zach", HairColor = "Blonde", Height = 66, Id = 5},
+                new Classmate {Name = "Tee", HairColor = "Black", Height = 66, Id = 6},
+                new Classmate {Name = "Seth", HairColor = "Brown", Height = 66, Id = 7},
+                new Classmate {Name = "Daniel", HairColor = "Brown", Height = 80, Id = 8}
+            };
+            return View(classmates);
+        }
+    }
+}
---
+++ Intro to MVC/Models/Classmate.cs
@@ -0,0 +1,10 @@
+﻿namespace Intro_to_MVC.Models
+{
+    public class Classmate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+        public int Height { get; set; }
+    }
+}
